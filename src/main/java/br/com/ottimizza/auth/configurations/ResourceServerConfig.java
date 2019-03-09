98a331e73c6f87b66367b1197cf3d9ff75cb7cd2
Lucas Martins <dev.lucasmartins@gmail.com>
2019-03-09 13:42:09 -0300

    removed some stuff from application.yml

--- src/main/java/br/com/ottimizza/auth/configurations/ResourceServerConfig.java
+++ src/main/java/br/com/ottimizza/auth/configurations/ResourceServerConfig.java
@@ -1,31 +1,21 @@
 package br.com.ottimizza.auth.configurations;
 
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
-@Order(40)
+@Order(40) // After SecurityConfig.
 @Configuration
 @EnableResourceServer
-public class ResourceServerConfig
-        extends ResourceServerConfigurerAdapter {
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        // http
-        //         .antMatcher("/user/info")
-        //         .authorizeRequests().anyRequest().authenticated();
-
-        http.csrf().disable();
-
         http
-        .authorizeRequests()
-            .antMatchers(HttpMethod.OPTIONS).permitAll()
-            .antMatchers(HttpMethod.OPTIONS, "/oauth/token").permitAll()
-            .antMatchers("/login", "/oauth/token").permitAll()
-            .anyRequest().permitAll();
+                .antMatcher("/user/info")
+                .authorizeRequests().anyRequest().authenticated();
     }
+
 }
--- src/main/java/br/com/ottimizza/auth/configurations/SecurityConfig.java
+++ src/main/java/br/com/ottimizza/auth/configurations/SecurityConfig.java
@@ -2,32 +2,22 @@ package br.com.ottimizza.auth.configurations;
 
 import br.com.ottimizza.auth.service.impl.UserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
-import javax.annotation.Resource;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
+@Order(39)
 @Configuration
 @EnableWebSecurity
-@Order(39)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -64,30 +54,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // http.csrf().disable()
-        //     .authorizeRequests()
-        //         .antMatchers(HttpMethod.OPTIONS).permitAll()
-        //         .antMatchers(HttpMethod.OPTIONS, "/oauth/token").permitAll()
-        //         .antMatchers("/login", "/oauth/token").permitAll()
-        //         .anyRequest().authenticated()
-        //         .and()
-        //         .formLogin().permitAll();
-
-        http
-            .csrf().disable()
-                //.exceptionHandling()
-                 //   .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
-            //.and()
-                .authorizeRequests()
-                    .antMatchers(HttpMethod.OPTIONS).permitAll()
-                    .antMatchers(HttpMethod.OPTIONS, "/oauth/token").permitAll()
-                    .antMatchers("/login", "/oauth/token").permitAll()
-                    .antMatchers("/**").authenticated()
-                    .anyRequest().authenticated()
+        http.csrf().disable()
+            .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/oauth/token").permitAll()
+                .antMatchers("/login", "/oauth/authorize", "/oauth/token").permitAll()
+                .anyRequest().authenticated()
                 .and()
-                    .formLogin().permitAll()
-                .and()
-                    .httpBasic();
+                .formLogin().permitAll();
     }
 
 }
