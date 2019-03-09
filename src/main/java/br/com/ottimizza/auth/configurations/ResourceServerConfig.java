f9d5eac8ba88cf06bee99db89f18f2673cc5368a
Lucas Martins <dev.lucasmartins@gmail.com>
2019-03-09 12:44:31 -0300

    removed some stuff from application.yml

--- src/main/java/br/com/ottimizza/auth/configurations/ResourceServerConfig.java
+++ src/main/java/br/com/ottimizza/auth/configurations/ResourceServerConfig.java
@@ -1,10 +1,12 @@
 package br.com.ottimizza.auth.configurations;
 
+import org.springframework.core.annotation.Order;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
+@Order(40)
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig
