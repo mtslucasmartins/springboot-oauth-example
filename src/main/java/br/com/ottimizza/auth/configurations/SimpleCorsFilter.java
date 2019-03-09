e8c79c2359854a37e933b3b92efeef88b00181f7
Lucas Martins <dev.lucasmartins@gmail.com>
2019-03-09 13:21:02 -0300

    removed some stuff from application.yml

--- src/main/java/br/com/ottimizza/auth/configurations/SimpleCorsFilter.java
+++ src/main/java/br/com/ottimizza/auth/configurations/SimpleCorsFilter.java
@@ -34,7 +34,7 @@ public class SimpleCorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, content-type, accept");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             System.out.println("SimpleCorsFilter.HttpServletResponse.SC_OK");
